4ac6c2db92b645546e93b78fa14ef22249c5f57c
codingchili <rduda@kth.se>
2019-11-21 20:39:31 +0100

    improved configuration features and format

--- main.py
+++ main.py
@@ -2,16 +2,29 @@ from lifxlan import *
 from aiocron import *
 import asyncio
 import yaml
+import json
+import math
 
 # yaml file with configuration.
 CONFIG_FILE = 'config.yaml'
 
-# the color used when fading to nighttime.
-COLOR_NIGHT = [0x0, 0x0, 0x0, 2500]
-    
+# default transition time when powering on/off or changing colors.
+DEFAULT_TRANSITION = 5
+
+HUE = 'hue'
+SATURATION = 'saturation'
+TRANSITION = 'transition'
+BRIGHTNESS = 'brightness'
+TEMPERATURE = 'temperature'
+POWER = 'power'
+NAME = 'name'
+
 
 def sec_to_ms(seconds):
     return seconds * 1000
+
+def ms_to_sec(ms):
+    return ms / 1000
 
 
 def log(line):
@@ -29,41 +42,85 @@ class CircadianLifx:
         lights = ','.join(map(lambda light: "'{}'".format(light.get_label()), lights))
         log("Discovered {}".format(lights))
 
-        self.fade_day = 900
-        self.fade_night = 300
-        self.color_default = [0xCBCB, 0xFFFF, 0xAFFF, 2500]
-        
         self.configure()
 
 
-    async def on(self, lamp):
-        log("powering up {} over {}s".format(lamp.get_label(), self.fade_day))
-        lamp.set_power(True)
-        #color = lamp.get_color()
-        #if (color[0] == 0x0):
-        #    color = self.color_default
-        color = self.color_default
-        lamp.set_color(color, sec_to_ms(self.fade_day))
+    async def on(self, lamp, schema):
+        transition = self.get_transition(schema)
+        log("powering up {} over {}s".format(schema[NAME], ms_to_sec(transition)))
+        lamp.set_power(True, transition)
+        await asyncio.sleep(ms_to_sec(transition))
 
 
-    async def off(self, lamp):
-        log("powering down {} over {}s.".format(lamp.get_label(), self.fade_night))
-        lamp.set_color(COLOR_NIGHT, sec_to_ms(self.fade_night))
-        await asyncio.sleep(self.fade_night)
-        lamp.set_power(False)
+    def get_transition(self, schema):
+        return sec_to_ms(schema.get(TRANSITION, DEFAULT_TRANSITION))
+
+
+    async def off(self, lamp, schema):
+        transition = self.get_transition(schema)
+        log("powering down {} over {}s.".format(schema[NAME], ms_to_sec(transition)))
+        lamp.set_power(False, transition)
+        await asyncio.sleep(ms_to_sec(transition))
+
 
     def configure(self):
         log("loading configuration from '{}'..".format(CONFIG_FILE))
         with open(CONFIG_FILE, 'r') as file:
             config = yaml.safe_load(file)
-
-            self.configure_schedule(config)
-            self.configure_fade(config)
-            self.configure_color(config)
+            self.configure_alarms(config)
             log('Configuration completed.')
 
 
-    def configure_schedule(self, config):
+    def set_color(self, lamp, schema):
+        current = lamp.get_color()
+     
+        hue = current[0]
+        saturation = current[1]
+        brightness = current[2]
+        temperature = current[3]
+        transition = self.get_transition(schema)
+
+        if (HUE in schema):
+            hue = schema[HUE] * 182
+
+        if (SATURATION in schema):
+            saturation = math.trunc(schema[SATURATION] * 256 * 256 -1)
+
+
+        if (BRIGHTNESS in schema):
+            brightness = math.trunc(schema[BRIGHTNESS] * 256 * 256 -1)
+
+        if (TEMPERATURE in schema):
+            temperature = schema[TEMPERATURE]
+
+        log("set color of lamp '{}' to [{}, {}, {}, {}] over transition {}s"
+                .format(schema[NAME], hue, saturation, brightness, temperature, ms_to_sec(transition)))
+
+        lamp.set_color([hue, saturation, brightness, temperature], transition)
+
+
+    async def process_event(self, lamp, schema):
+        keys = [HUE, SATURATION, BRIGHTNESS, TEMPERATURE]
+
+        log("processing event: {}".format(json.dumps(schema)))
+
+        color_updated = False
+        for key in keys:
+            if key in schema:
+                color_updated = True
+
+        update_color = lambda: self.set_color(lamp, schema) if color_updated else None
+
+        if (POWER in schema):
+            if (schema[POWER]):     
+                update_color()
+                await self.on(lamp, schema)
+            else:
+                await self.off(lamp, schema)
+                update_color()
+
+
+    def configure_alarms(self, config):
         log('scheduling lamps..')
         for lamp in config['lamps']:
             schemas = lamp['schema']
@@ -72,32 +129,15 @@ class CircadianLifx:
             lamp = self.lifx.get_device_by_name(lamp['name'])
 
             for schema in schemas:
+                schema['name'] = lamp.get_label()
                 cron = schema['cron']
                 power = schema['power']
                 print("\t\t- scheduled at '{}', power={}".format(cron, power))
+                crontab(cron, func=lambda: self.process_event(lamp, schema), start=True)
 
-                if (power):
-                    crontab(cron, func=lambda: self.on(lamp), start=True)
-                else:
-                    crontab(cron, func=lambda: self.off(lamp), start=True)
-
-
-    def configure_fade(self, config):
-        fade = config['fade']
-        self.fade_day = fade['day']
-        self.fade_night = fade['night']
-        log('configuring fade timers')
-        print("\t\t- night->day set to {}s.".format(self.fade_day))
-        print("\t\t- day->night set to {}s.".format(self.fade_night))
-
-
-    def configure_color(self, config):
-        configured_color = config['default_color']
-        if (configured_color is not None):
-            self.color_default = config['default_color']
-            log("default color configured {}".format(self.color_default))
-        else:
-            log("default color not configured using {}".format(self.color_default))            
+                # dev
+                if (schema['name'] == 'LIFX Flory'):
+                    crontab("* * * * * 0,15,30,45", func=lambda: self.process_event(lamp, schema), start=True)
 
 
 try:
